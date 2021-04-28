8b28ebe17bdf12a49305252e6d72f2b023203472
pongpetch <pongpet007@gmail.com>
2021-04-28 08:49:27 +0700

    update title

--- components/Head_meta.js
+++ components/Head_meta.js
@@ -4,9 +4,9 @@ export default function Head_meta({title,keyword,description}) {
     return (
         <div>
             <Head>
-                <title>{title}</title>
-                <meta name="description" content={keyword} />
-                <meta name="keywords" content={description} />
+                <title>{title?title:'ค้นหาสินทรัพย์น่าซื้อ ผ่านเว็บที่เชื่อถือได้ HOUSINGHOME.NET'}</title>
+                <meta name="description" content={keyword?keyword:''} />
+                <meta name="keywords" content={description?description:''} />
                 <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"></script>
             </Head>
         </div>
